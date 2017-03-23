f47d6f3320d8ffaee0eb60ee7007823d42839452
maiger <mage_ege@hotmail.com>
2017-03-23 18:05:35 +0200

    Add reset button

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    public void Reset()
+    {
+        player.transform.position = spawnPoint.transform.position;
+        player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+    }
+}
